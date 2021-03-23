df6eced43fc286632b7b5ae8a94121cd52a88f63
ari-barnett <72417784+ari-barnett@users.noreply.github.com>
2021-03-23 16:18:31 -0400

    Update visual_execution.py

--- ABM/visual_execution.py
+++ ABM/visual_execution.py
@@ -1,19 +1,23 @@
 from matplotlib import pyplot, colors
 import numpy as np
 import ABM_tools 
+import statistics
 
 lx = []
 ly = []
+lz = []
 i = 0 
+
 
 print('Inital Population: ', np.sum(ABM_tools.x))
 
 """ EXECUTION OF ABM SYSTEM """
 ''' ==============================================================  '''
 while i < 365: 
-    ABM_tools.survival(0.5)
+    ABM_tools.survival(0.50)
     ABM_tools.reproduce()
     print('Day',i ,'Cells Reproduced: ',np.count_nonzero(ABM_tools.new == 2))
+    lz.append(np.count_nonzero(ABM_tools.new == 2))
     final = np.where(ABM_tools.new == 2, 1, ABM_tools.new )
     lx.append(i)
     ly.append(np.sum(final))
@@ -26,7 +30,7 @@ while i < 365:
 
 """ VISUALIZATION """
 ''' ==============================================================  '''
-pyplot.title('Decay Model - Survival Rate = 0.50')
+pyplot.suptitle('Activated Cell Population x Time - 365 Days -- Survival Rate: 0.50')
 pyplot.xlabel('Time (Days)')
 pyplot.ylabel('Activated Cell Poplulation')
 q = np.array(lx)
@@ -34,4 +38,12 @@ w = np.array(ly)
 m, b = np.polyfit(q, w, 1)
 pyplot.plot(q, m*q + b, color='#949494', linestyle='dashed')
 pyplot.plot(lx, ly, color='#5866d1')
+
+print('')
+print('END OF SIMULATION')
+print('=============================================')
+print('Standard Deviation of Cells Born: ', statistics.stdev(lz))
+print('Average Cell Birth: ', statistics.mean(lz))
+print('=============================================')
+
 pyplot.show()
